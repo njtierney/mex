41c7a671666ddc579b531085250d579be7b1ad0e
tierneyn <nicholas.tierney@gmail.com>
2014-09-09 19:24:46 +1000

    first attempt at adding code to mex
    Here I added the section that performs the hierarchical clustering then
    rpart CART modelling. It runs!  I can use `mex.data.frame(sim.data)`
    and it actually performs the hierarchical clustering, then the rpart
    then the rpart plotting.
    
    However, there are still problems.
    1. It prints the entire output of everything that happens when I run
    mex.data.frame()
    2. I need to sort out my classes so that I can plot my rpart tree
    3. I need to keep thinking about naming things and style.
    4. I need to learn more about S3 classes.

--- R/mex.R
+++ R/mex.R
@@ -20,14 +20,64 @@ mex <- function(...)
 # ============================================================================
 
 #' @export
-mex.data.frame <- function(d, ...)
+mex.data.frame <- function(data, ...)
 {
-    # Do stuff here...
 
-    # Create a `mex` object and return.
-    res <- d
-    class(res) <- c("mex", class(res))
-    return(res)
+  # ========================
+  # hierarchical clustering
+  # ========================
+  
+    # perform clustering on a dataset 
+    
+    # get the data, then make it an na.matrix, then calculate euclidean distance
+    # then perform hierarchical clustering
+    
+    hclust.fit <- data %>%
+      is.na.data.frame %>%
+      dist %>%
+      hclust
+    
+    # use hclust.fit, cut it into 4 pieces, then make it a factor
+    c.id  <- hclust.fit %>%
+      cutree(4) %>%
+      as.factor
+    
+    ## add the 4 pieces into a dataframe
+    hclust.mex <- data.frame(data, 
+                            c.id)
+    
+  # ======
+  # rpart
+  # ======
+  
+    # run the rpart model, using all variables (specified by . ) 
+    rpart.mex <- rpart(c.id ~ . ,
+                      data = hclust.mex,
+                      na.action = na.rpart, 
+                      method = "class")
+    
+    ## plot the rpart tree - currently doesn't allow us to plot it ourselves
+    rpart.plot <- prp(rpart.mex, 
+                      extra = 1, 
+                      type = 4, 
+                      prefix = "Miss Clust = ")
+  
+  # =======
+  # output
+  # =======
+  
+  structure(list(
+    hclust.mex = hclust.mex,
+    rpart.mex = rpart.mex,
+    rpart.plot = rpart.plot
+    ), class = "mex")
+  
+#     # Create a `mex` object and return.
+#     res <- data
+#     # make res gain an additional class, mex
+#     class(res) <- c("mex", class(res))
+#   
+#     return(res)
 }
 
 #' @export
