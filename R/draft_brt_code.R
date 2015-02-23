62d0a03f9e303a7bf857dc706b3a55b929efe932
tierneyn <nicholas.tierney@gmail.com>
2015-02-23 23:57:37 +1000

    Fixing description extras
    Fixing author order; adding version numbers to all imported packages
    (as per hadley’s instructions.

--- R/draft_brt_code.R
+++ R/draft_brt_code.R
@@ -1,61 +1,38 @@
-##
-### fit the model using the BRT
-##
-## I'm interested to see the differences in fitting the model using gbm.step
-## and regular old gbm
-
-data(sim.dat)
-
-library(gbm)
-mex.brt = gbm(c.id ~ C1 + C2 + C3 + F1 + F2, 
-              data = sim.dat, 
-              distribution = "multinomial", 
-              n.trees = 5000, 
-              shrinkage = 0.01, 
-              bag.fraction = 0.5,
-              interaction.depth = 2)
-
-summary(mex.brt)
-
-names(mex.brt)
-
-## THIS is concerning.  When I run the results with the original gbm.step
-## function, It tells me that C1 was the most important
-
-mex.brt.2 <- gbm(miss_perc ~ C1 + C2 + C3 + F1 + F2, 
-                 data = sim.dat, 
-                 distribution = "gaussian", 
-                 n.trees = 5000, 
-                 shrinkage = 0.01, 
-                 bag.fraction = 0.5,
-                 interaction.depth = 2)
-
-summary(mex.brt)
-
-## It looks like we cannot use the code from elith et al., as it does not
-## have a specification for multinomial distributions.
-## 
-
-## It ALSO looks like I get VERY different results when using the gbm.step()
-## compared to the gbm() function.
-## I'm glad that I've found this error, but this may have created more work 
-## for me.
-## It is now time to delve into 'the source', to find out just what is so 
-## different in gbm() and gbm.step().
-
-## source code from elith et al.
-source("~/Dropbox/ALL THE THINGS/PhD/Guides/Elith_BRT_Guide_2_of_2/brt.functions.R")
-
-head(sim.dat)
-
-brt.mod <- gbm.step(data= sim.dat, 
-                    gbm.x = c(2:6), 
-                    gbm.y = 10,
-                    family = "gaussian",
-                    tree.complexity = 2,
-                    learning.rate = 0.01,
-                    bag.fraction = 0.5)
-
-summary(brt.mod)
+# ##
+# ### fit the model using the BRT
+# ##
+# ## I'm interested to see the differences in fitting the model using gbm.step
+# ## and regular old gbm
+# 
+# data(sim.dat)
+# 
+# library(gbm)
+# mex.brt = gbm(c.id ~ C1 + C2 + C3 + F1 + F2, 
+#               data = sim.dat, 
+#               distribution = "multinomial", 
+#               n.trees = 5000, 
+#               shrinkage = 0.01, 
+#               bag.fraction = 0.5,
+#               interaction.depth = 2)
+# 
+# summary(mex.brt)
+# 
+# names(mex.brt)
+# 
+# ## THIS is concerning.  When I run the results with the original gbm.step
+# ## function, It tells me that C1 was the most important
+# 
+# mex.brt.2 <- gbm(miss_perc ~ C1 + C2 + C3 + F1 + F2, 
+#                  data = sim.dat, 
+#                  distribution = "gaussian", 
+#                  n.trees = 5000, 
+#                  shrinkage = 0.01, 
+#                  bag.fraction = 0.5,
+#                  interaction.depth = 2)
+# 
+# summary(mex.brt)
+# 
+# ## Cannot use the code from elith et al., as it does not have a specification
+# ## for multinomial distributions.
 
 
--- R/gen_data_mcar.R
+++ R/gen_data_mcar.R
@@ -1,14 +1,28 @@
-## this is the function that creates the MCAR data.
-
-###########
+#' Create MCAR missingness in a dataframe.
+#' 
+#' \code{gen_data_mcar} takes a dataframe and adds in missingness, 
+#'                      returning the original, and newdata
+#'
 #' @param data      = data you want to inflict the missingness on
 #' @param miss.perc = percent of missing data you want (approximately)
-#' @param seed =   set the random seed so that the results can be replicated.
-#' @param col.num = range of column numbers to set to random. Could be improved.
+#' @param seed      = set the random seed for reproducibility.
+#' @param col.num   = range of column numbers to set to random missingness.
+#'                    This could be improved.
+#'                    
+#' @return this function should return a dataframe
+#' 
+#' @examples
+#' dat.mcar <- gen_data_mcar(data = iris, 
+#'                           miss.perc =  0.5,
+#'                           seed = 1234,
+#'                           col.num = c(1:3))
+#'                    
+
+
 gen_data_mcar <- function(data, 
-                       miss.perc,
-                       seed,
-                       col.num){
+                          miss.perc,
+                          seed,
+                          col.num){
   
   ## create a working copy of the data
     newdata <- data
@@ -16,20 +30,23 @@ gen_data_mcar <- function(data,
   ## set the % missing data
     newdata$prob <- miss.perc
   
+  ## set the seed
   set.seed(seed)
+    
+    # for each row in the dataset, go through each j-th specified column
     for (i in (1:nrow(newdata))){
       
       for (j in (col.num)){
         
-        ## make a random uniform for sim1, 
+        ## make a random uniform for the data, 
         newdata$runif <- runif(nrow(newdata),  ## for all rows
                                min = 0, 
                                max = 1) ## set a probability between 0 and 1.
         
-        ## IF runif is equal to or smaller than prob, make it missing (NA)
-        
+        ## IF runif is equal to or smaller than prob, 
         if (newdata$runif[i] <= newdata$prob[i]) { ## open if-else
           
+          ## THEN make that element missing
           newdata[i, j] <- NA
           
         } ## close if-else
@@ -55,15 +72,14 @@ mcar.dat <- select(newdata,
     for (m in (1:nrow(mcar.dat))){
       
       ## finds how many observations are missing 
-        ## sum(is.na(data[row,]))
-      
       ## divides by the # of observations in that row
-        ## (ncol(data))
+      ## makes a row equal to the % missing data.
       
-      ## makes a row equal to the % missing data.    
-        ## (miss_perc)
-      
-      mcar.dat$miss_perc[m] <- sum(is.na(mcar.dat[m, ])) / ncol(mcar.dat)
+      mcar.dat$miss_perc[m] <- 
+          ## finds how many observations are missing, 
+          ## divides by the # of observations in that 
+          ## row makes a row equal to the % missing data.
+        sum(is.na(mcar.dat[m, ])) / ncol(mcar.dat)
       
     }
 
